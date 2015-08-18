7b9023ef4d1ba7341212022cf7e9ed1b991b2a77
keerthanats <keerthanats@gmail.com>
2015-08-18 11:22:11 +0530

    hello

---
+++ Fragment/app/src/main/java/com/example/hp/fragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hp.fragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity implements MyListFragment.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRssItemSelected(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        if (fragment != null && fragment.isInLayout()) {
+            fragment.setText(link);
+        }
+    }
+
+} 
